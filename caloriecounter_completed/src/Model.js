7f9ce7c4c1bd7cd704ec1e09cb8bffa7d9f626e9
Oleg Abrazhaev <seyferseed@gmail.com>
2020-07-27 11:57:52 +0200

    fix cs and start model changes

--- caloriecounter_completed/src/Model.js
+++ caloriecounter_completed/src/Model.js
@@ -1,10 +1,16 @@
 const initModel = {
-  description: 'Breakfast',
-  calories: 480,
-  showForm: false,
-  nextId: 0,
-  editId: null,
-  meals: [],
+    description: 'Breakfast',
+    calories: 480,
+    showForm: false,
+    nextId: 0,
+    editId: null,
+    meals: [],
+    form: {
+        description: 'Breakfast',
+        calories: 480,
+        show: false,
+        editId: null,
+    },
 };
 
 export default initModel;
